74c17579f28709ec0de0f15831f5e6e6f5046684
UsamaA7med <osamaahmedwork1@gmail.com>
2026-06-05 22:06:24 +0300

    reponsive changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,19 +4,19 @@ import { AsidebarSheet } from './asaidbar/AsidebarSheet'
 const Header = () => {
   return (
     <header className="flex items-center justify-between h-16 px-4 xl:px-10 border-b bg-primary/80 backdrop-blur-xl shadow-[0_0_20px_0_rgba(208,188,255,0.15)] w-full">
-      <div className="flex items-center gap-4 flex-1 min-w-0">
+      <div className="flex items-center gap-[24px] flex-1 min-w-0">
         <AsidebarSheet />
 
         <h1 className="text-[24px] text-secondary font-bold shrink-0 whitespace-nowrap">
           Wizarding Registry
         </h1>
 
-        <div className="hidden md:flex flex-1 min-w-0 max-w-md xl:pl-4">
-          <div className="rounded-full bg-[#2736474D] h-9 flex items-center gap-2 px-3 w-full min-w-0">
+        <div className="hidden md:flex w-[256px] xl:pl-[80px]">
+          <div className="rounded-full bg-[#2736474D] h-9 flex items-center gap-2 px-3 w-full">
             <img src="Icon.png" className="w-[10.5px] h-[10.5px] shrink-0" />
             <Input
               placeholder="Scrying records..."
-              className="border-0 bg-transparent shadow-none focus-visible:ring-0 focus-visible:ring-offset-0 min-w-0"
+              className="border-0 bg-transparent shadow-none focus-visible:ring-0 focus-visible:ring-offset-0"
             />
           </div>
         </div>
--- src/components/asaidbar/AsidebarRegistry.tsx
+++ src/components/asaidbar/AsidebarRegistry.tsx
@@ -1,11 +1,10 @@
 const AsidebarRegistry = () => {
   return (
     <div className="flex flex-col items-center px-6 justify-center pb-20">
-      <div className="rounded-xl bg-linear-to-r from-[#D0BCFF] to-[#FFB95F] p-px">
-        <img
-          src="stars.png"
-          className="border-radial-[#D0BCFF,#FFB95F] border p-5 rounded-[12px] w-15.5 h-15.5 bg-[#0D1C2D]"
-        />
+      <div className="rounded-[12px] p-px bg-gradient-to-br from-[#D0BCFF] to-[#FFB95F]">
+        <div className="rounded-[10px] bg-[#0D1C2D] w-[60px] h-[60px] flex justify-center items-center">
+          <img src="stars.png" className="w-[27.5px] h-[27.5px]" />
+        </div>
       </div>
       <p className="text-secondary text-[16px] font-extrabold pt-2">Registry</p>
       <p className="text-[12px] font-medium tracking-[0.6px] text-[#CBC3D7] opacity-70 text-center">
